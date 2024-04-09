e0cde0e4163bc82a4c822633473590f9b7f844df
bermog <code@bermog.com>
2024-04-09 18:47:34 +0200

    Add neogit keymaps

--- nvim/.config/nvim/lua/user/plugins/neogit.lua
+++ nvim/.config/nvim/lua/user/plugins/neogit.lua
@@ -5,5 +5,11 @@ return {
     "sindrets/diffview.nvim",
     "nvim-telescope/telescope.nvim",
   },
+  keys = {
+      { '<leader>gs', '<Cmd>Neogit<CR>' },
+      { '<leader>gc', '<Cmd>Neogit commit<CR>' },
+      { '<leader>gp', '<Cmd>Neogit pull<CR>' },
+      { '<leader>gP', '<Cmd>Neogit push<CR>' },
+  },
   config = true
 }
