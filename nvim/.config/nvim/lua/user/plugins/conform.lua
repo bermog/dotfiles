8d2cd775b829d0803c7ea53729a270fbc98dd448
bermog <code@bermog.com>
2024-04-21 20:36:25 +0200

    Add conform plugin to nvim with php formatter

---
+++ nvim/.config/nvim/lua/user/plugins/conform.lua
@@ -0,0 +1,33 @@
+return {
+    'stevearc/conform.nvim',
+    lazy = true,
+    event = { 'BufReadPre', 'BufNewFile' },
+    keys = {
+        {
+            '<leader>f', function()
+                require('conform').format({
+                    lsp_fallback = true,
+                    async = false,
+                    timeout_ms = 1000,
+                })
+            end
+        },
+    },
+    opts = {
+        formatters_by_ft = {
+            php = { 'php-cs-fixer' },
+        },
+        formatters = {
+            ['php-cs-fixer'] = {
+                command = 'php-cs-fixer',
+                args = {
+                    'fix',
+                    '--rules=@PSR12',
+                    '$FILENAME',
+                },
+                stdin = false,
+            },
+        },
+        notify_on_error = true,
+    },
+}
