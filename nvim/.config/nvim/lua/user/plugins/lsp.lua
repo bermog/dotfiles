59c1483f542fdcb2c14042786c9e502cf8f49d3b
bermog <code@bermog.com>
2024-04-21 21:04:55 +0200

    Create custom nvim snippets, update nvim-cmp config to use them

--- nvim/.config/nvim/lua/user/plugins/lsp.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp.lua
@@ -22,7 +22,8 @@ return {
         'hrsh7th/nvim-cmp',
         event = 'InsertEnter',
         dependencies = {
-            {'L3MON4D3/LuaSnip'},
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
         },
         config = function()
             -- Here is where you configure the autocompletion settings.
@@ -33,6 +34,10 @@ return {
             local cmp = require('cmp')
             local cmp_select = { behavior = cmp.SelectBehavior.select }
 
+            -- Load custom snippets
+            local luasnip = require('luasnip')
+            require('luasnip.loaders.from_snipmate').lazy_load()
+
             cmp.setup({
                 formatting = lsp_zero.cmp_format({details = true}),
                 mapping = cmp.mapping.preset.insert({
@@ -40,14 +45,34 @@ return {
                     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
                     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                    ['<C-n>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                    ['<C-p>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
                 }),
                 snippet = {
                     expand = function(args)
-                        require('luasnip').lsp_expand(args.body)
+                        luasnip.lsp_expand(args.body)
                     end,
                 },
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                }),
             })
         end
     },
