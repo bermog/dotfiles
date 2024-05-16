c228214b30668433f5dfb6cebfa611a2622d983e
bermog <code@bermog.com>
2024-05-16 23:31:28 +0200

    Lock neogit to the last commit supporting nvim 0.9

--- nvim/.config/nvim/lua/user/plugins/neogit.lua
+++ nvim/.config/nvim/lua/user/plugins/neogit.lua
@@ -1,15 +1,16 @@
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "sindrets/diffview.nvim",
-    "nvim-telescope/telescope.nvim",
-  },
-  keys = {
-      { '<leader>gs', '<Cmd>Neogit<CR>' },
-      { '<leader>gc', '<Cmd>Neogit commit<CR>' },
-      { '<leader>gp', '<Cmd>Neogit pull<CR>' },
-      { '<leader>gP', '<Cmd>Neogit push<CR>' },
-  },
-  config = true
+    "NeogitOrg/neogit",
+    commit = "bc0c609",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
+    keys = {
+        { '<leader>gs', '<Cmd>Neogit<CR>' },
+        { '<leader>gc', '<Cmd>Neogit commit<CR>' },
+        { '<leader>gp', '<Cmd>Neogit pull<CR>' },
+        { '<leader>gP', '<Cmd>Neogit push<CR>' },
+    },
+    config = true
 }
