59bec0e8f09930c167d16c83b16b2b6e13a1f350
bermog <code@bermog.com>
2024-05-19 00:05:29 +0200

    Update fish abbreviations

--- fish/.config/fish/conf.d/abbreviations.fish
+++ fish/.config/fish/conf.d/abbreviations.fish
@@ -1,2 +1,23 @@
-abbr -ag vim nvim
-abbr -ag rg 'rg --hidden'
+# Tools
+abbr -ag vi nvim
+abbr -ag cat bat
+
+# Filesystem
+abbr -ag ll 'ls -la'
+abbr -ag tr 'tree -a'
+
+# Git
+abbr -ag gs 'git status'
+abbr -ag gb 'git branch'
+abbr -ag gl 'git log'
+abbr -ag ga 'git add'
+abbr -ag gc 'git commit'
+abbr -ag gco 'git checkout'
+abbr -ag gp 'git pull'
+abbr -ag gps 'git push'
+abbr -ag gr 'git reset'
+abbr -ag grm 'git remote'
+
+# Wireguard
+abbr -ag wu 'wg-quick up'
+abbr -ag wd 'wg-quick down'
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     bind -M insert \cf forward-word
-    bind -M insert \cr end-of-line
+    bind -M insert \ce end-of-line
 
     starship init fish | source
 end
